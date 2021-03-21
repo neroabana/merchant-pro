f6629693a38d54f399df41ca4425d9c9e884f4f8
abana <neroabana@gmail.com>
2021-03-21 12:33:45 +0100

    sanitized line

--- src/line.ts
+++ src/line.ts
@@ -1,12 +1,12 @@
 import { Bike } from "./bike";
 
-export class Line {
-    constructor(bike: Bike, quantity: number, coupon = "") {
-        this.bike = bike;
-        this.quantity = quantity;
-        this.coupon = coupon;
-    }
-    public bike: Bike;
-    public quantity: number;
-    public coupon: string;    
+export class Line {  
+  constructor(bike: Bike, quantity: number, coupon = "") {
+      this.bike = bike;
+      this.quantity = quantity;
+      this.coupon = coupon;
+  }
+  public bike: Bike;
+  public quantity: number;
+  public coupon: string;    
 }
