8a341ca8450480a282584e46b82350d078a19fb9
abana <neroabana@gmail.com>
2021-03-24 23:15:12 +0100

    fixed bike constructor

--- src/bike.ts
+++ src/bike.ts
@@ -6,9 +6,9 @@ export class Bike {
     static FiveThousand = 5000;    
     
     constructor(brand: string, model: string, price: number) {
-        this.brand = brand;
-        this.model = model;
-        this.price = price;
+      this.brand = brand;
+      this.model = model;
+      this.price = price;
     }
 
     public brand: string;
