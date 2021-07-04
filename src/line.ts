cc6f90b444316f8471a298efbf786d912a1d2f78
abana <neroabana@gmail.com>
2021-07-04 09:48:41 +0100

    optimized line constructor

--- src/line.ts
+++ src/line.ts
@@ -2,9 +2,9 @@ import { Bike } from "./bike";
 
 export class Line {  
   constructor(bike: Bike, quantity: number, coupon = "") {
-      this.bike = bike;
-      this.quantity = quantity;
-      this.coupon = coupon;
+    this.bike = bike;
+    this.quantity = quantity;
+    this.coupon = coupon;
   }
   public bike: Bike;
   public quantity: number;
