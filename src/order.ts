f93c4baeee14bf25060c3e6a4cc43ea65d55c413
abana <neroabana@gmail.com>
2021-08-14 08:04:16 +0100

    entersed error block

--- src/order.ts
+++ src/order.ts
@@ -7,9 +7,7 @@ function formatMoney(amount: any, decimalCount = 2, decimal = ".", thousands = "
     try {      
         decimalCount = Math.abs(decimalCount);
         decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
-
         const negativeSign = amount < 0 ? "-" : "";
-
         let i: any = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
         let j = (i.length > 3) ? i.length % 3 : 0;
 
