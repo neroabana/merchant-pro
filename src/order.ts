0a693eedee34eac70759a88ab3d5d5ca425df07c
abana <neroabana@gmail.com>
2021-09-29 15:16:47 +0100

    refactored order

--- src/order.ts
+++ src/order.ts
@@ -6,6 +6,7 @@ import { Bike } from "./bike";
 const newline = '\n';
 
 function formatMoney(amount: any, decimalCount = 2, decimal = ".", thousands = ",") {
+  
     try {      
         decimalCount = Math.abs(decimalCount);
         decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
